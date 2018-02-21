9fe8828cb3a37e06c9ef87c7bc45fe3abea83116
concept-x <emilie_elliotthotmail.com>
2018-02-20 21:31:21 -0600

    Models cp4 assignment: complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -6,7 +6,6 @@ class AddressBook
     
     def initialize
         @entries = []
-        puts "I am here"
     end
 
 
@@ -15,12 +14,25 @@ def add_entry(name, phone_number, email)
     index = 0
     entries.each do |entry|
         # #10 loop to insert entry name in ABC order
-        if name < entry.name #if 'name' precedes 'entry.name, insert entry and break
+        if name < entry.name #if 'entry.name' (line 15) precedes 'name' (line 12), insert entry and break...
             break
         end
-        index+=1 # #10 cont'd: move to next index and check again
+        index+=1 # #10 cont'd: ...otherwise, move to next index and check again
     end
     # #11 insert new entry into 'entries' using index
     entries.insert(index, Entry.new(name, phone_number, email))
+end
+
+
+#assignment: remove_entry method
+    def remove_entry(name, phone_number, email)
+        index = 0 #var assignment, as in add_entry
+        #iterate over entries to find match; use index to remove. 
+        entries.each do |entry|
+            if name == entry.name #if name to delete == entry.name
+                entries.delete_at(index) #index var defined at line 29
+            end
+            index+=1 
+        end
     end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -29,13 +29,26 @@ RSpec.describe AddressBook do
         
         it "adds the correct information to entries" do
             book = AddressBook.new
-            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com') #book has to include entry to remove one
             new_entry = book.entries[0]
             
             #multiple 'expects' mean if any fail, the whole 'it' test fails
+            #these are all properties of the entry (line 32)
             expect(new_entry.name).to eq('Ada Lovelace')
             expect(new_entry.phone_number).to eq('010.012.1815')
             expect(new_entry.email).to eq('augusta.king@lovelace.com')
+        end
+    end
+    
+    #assignment: remove_entry
+    describe "#remove_entry" do
+        it "removes only one entry from the address book" do
+            book = AddressBook.new
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')#add because you need >= 1 entry
+            book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+                        
+            #evaluate entries.size before and after removal to ensure only one entry was deleted? Yes, but unnecessary in this case due to line 51
+            expect(book.entries.size).to eq(0)#since we only put in one entry, after we remove one, the total should be zero.
         end
     end
 end
